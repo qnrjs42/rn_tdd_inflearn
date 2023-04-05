d0b25a90f333259e3db167c8dc3f48988b6365fd
qnrjs42 <qnrjs42@naver.com>
2023-04-06 01:23:43 +0900

    @testing-library

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -1,12 +1,15 @@
-// __tests__/App-test.tsx
-
-import 'react-native';
-import React from 'react';
-import App from '../App';
+import {Text} from 'react-native';
+import {render, screen} from '@testing-library/react-native';
 
 describe('Jest', () => {
   it('is it working?', () => {
     const a = 1;
     expect(a + 1).toBe(2);
   });
+});
+
+test('examples of some things', async () => {
+  render(<Text>Hello</Text>);
+
+  expect(screen.toJSON()).toMatchSnapshot();
 });
