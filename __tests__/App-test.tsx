72e684fe27f561a099cf813be0af3954aad941ac
qnrjs42 <qnrjs42@naver.com>
2023-04-06 00:32:22 +0900

    init

---
+++ __tests__/App-test.tsx
@@ -0,0 +1,12 @@
+// __tests__/App-test.tsx
+
+import 'react-native';
+import React from 'react';
+import App from '../App';
+
+describe('Jest', () => {
+  it('is it working?', () => {
+    const a = 1;
+    expect(a + 1).toBe(2);
+  });
+});
